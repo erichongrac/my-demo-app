713349a24a8dbb454ac3dcdde24e86fe177ef435
erichongrac <148745337+erichongrac@users.noreply.github.com>
2025-05-16 22:59:59 +0800

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,5 @@
 # GitHub provider config
 provider "github" {
-  token = var.github_token != "" ? var.github_token : getenv("GH_TOKEN")
-  owner = var.github_owner != "" ? var.github_owner : getenv("GH_OWNER")
+  token = var.github_token
+  owner = var.github_owner
 }
