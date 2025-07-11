10b4215a5153c0a318645ba2574d83f80ba249c7
erichongrac <148745337+erichongrac@users.noreply.github.com>
2025-07-11 16:18:51 +0800

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'export',
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    // NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_API_BASE_URL: "https://demo-backend-app.azurewebsites.net"
   },
 };
 
